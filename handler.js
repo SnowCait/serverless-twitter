e1b32cf51683c385e7a97cd3ecccad01dd436bff
SnowCait <SnowCait@users.noreply.github.com>
2022-11-10 04:46:44 +0900

    Log

--- handler.js
+++ handler.js
@@ -21,40 +21,45 @@ module.exports.hello = async (event) => {
 };
 
 module.exports.auth = async (event, context, callback) => {
-  const { code, verifier } = JSON.parse(event.body);
+  return new Promise(async (resolve, reject) => {
+    const { code, verifier } = JSON.parse(event.body);
+    console.log('[params]', code, verifier);
 
-  const secret_name = "TwitterClientSecret";
-  const response = await secretsManager.getSecretValue({
-    SecretId: secret_name,
-  }).promise();
+    const secret_name = "TwitterClientSecret";
+    const response = await secretsManager.getSecretValue({
+      SecretId: secret_name,
+    }).promise();
+    console.log('[secret]', response);
 
-  const url = 'https://api.twitter.com/2/oauth2/token';
-  const clientId = 'NnduMzZ5bnk4T1RfT21BdkJlZUg6MTpjaQ';
-  const clientSecret = response.SecretString;
-  const redirectUrl = 'http://localhost/';
-  const request = await https.request(url, {
-    method: 'POST',
-    headers: {
-      'Authorization': 'Basic ' + Buffer.from(`${clientId}:${clientSecret}`).toString('base64'),
-      'Content-Type': 'application/json',
-    },
-  }, response => {
-    console.log('[access token]', response);
-    response.setEncoding('utf8');
-    response.on('data', chunk => {
-      callback(chunk, response.statusCode);
+    const url = 'https://api.twitter.com/2/oauth2/token';
+    const clientId = 'NnduMzZ5bnk4T1RfT21BdkJlZUg6MTpjaQ';
+    const clientSecret = response.SecretString;
+    const redirectUrl = 'http://localhost/';
+    const request = await https.request(url, {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Basic ' + Buffer.from(`${clientId}:${clientSecret}`).toString('base64'),
+        'Content-Type': 'application/json',
+      },
+    }, response => {
+      console.log('[access token]', response);
+      response.setEncoding('utf8');
+      response.on('data', chunk => {
+        console.log('[data]', chunk);
+        resolve(chunk);
+      });
+    }).on('error', error => {
+      reject(Error(error));
     });
-  }).on('error', error => {
-    callback(Error(error));
+
+    request.write(JSON.stringify({
+      code,
+      grant_type: 'authorization_code',
+      client_id: clientId,
+      redirect_uri: redirectUrl,
+      code_verifier: verifier,
+    }));
+
+    request.end();
   });
-
-  request.write(JSON.stringify({
-    code,
-    grant_type: 'authorization_code',
-    client_id: clientId,
-    redirect_uri: redirectUrl,
-    code_verifier: verifier,
-  }));
-
-  request.end();
 };
