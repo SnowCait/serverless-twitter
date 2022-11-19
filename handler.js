1e4c32c51991b3742a5e188e76b2b63f3fa2cb76
SnowCait <SnowCait@users.noreply.github.com>
2022-11-19 23:12:25 +0900

    Log

--- handler.js
+++ handler.js
@@ -9,6 +9,7 @@ const secretsManager = new AWS.SecretsManager({
 module.exports.hello = async (event) => {
   console.log(event);
   console.log(process.versions);
+  console.log(process);
   return {
     statusCode: 200,
     body: JSON.stringify(
