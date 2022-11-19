02b870bd5f8d9fd9762e5e41b4a2a36aab1db638
SnowCait <SnowCait@users.noreply.github.com>
2022-11-19 23:40:54 +0900

    fetch

--- handlers/public.js
+++ handlers/public.js
@@ -4,6 +4,8 @@ module.exports.hello = async (event) => {
   console.log(event);
   console.log(process);
   console.log(process.moduleLoadList);
+  const response = await fetch('https://snowcait.github.io/serverless-twitter/');
+  console.log(await response.text());
   return {
     statusCode: 200,
     body: JSON.stringify(
