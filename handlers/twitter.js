37d58c0ce316349e81e5796e5f0f69284eba29a6
SnowCait <SnowCait@users.noreply.github.com>
2022-11-20 01:46:46 +0900

    Reduce nest

--- handlers/twitter.js
+++ handlers/twitter.js
@@ -5,35 +5,33 @@ const { SecretsManagerClient, GetSecretValueCommand } = require('@aws-sdk/client
 const secretsManager = new SecretsManagerClient({ region: 'ap-northeast-1' });
 
 module.exports.auth = async event => {
-  return new Promise(async (resolve, reject) => {
-    const { code, verifier, redirectUrl } = JSON.parse(event.body);
+  const { code, verifier, redirectUrl } = JSON.parse(event.body);
 
-    const command = new GetSecretValueCommand({ SecretId: 'TwitterClientSecret' });
-    const secrets = await secretsManager.send(command);
-    console.log('[secrets]', secrets);
+  const command = new GetSecretValueCommand({ SecretId: 'TwitterClientSecret' });
+  const secrets = await secretsManager.send(command);
+  console.log('[secrets]', secrets);
 
-    const url = 'https://api.twitter.com/2/oauth2/token';
-    const clientId = 'NnduMzZ5bnk4T1RfT21BdkJlZUg6MTpjaQ';
-    const { TWITTER_CLIENT_SECRET: clientSecret } = JSON.parse(secrets.SecretString);
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Authorization': 'Basic ' + Buffer.from(`${clientId}:${clientSecret}`).toString('base64'),
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        code,
-        grant_type: 'authorization_code',
-        redirect_uri: redirectUrl,
-        code_verifier: verifier,
-      }),
-    });
+  const url = 'https://api.twitter.com/2/oauth2/token';
+  const clientId = 'NnduMzZ5bnk4T1RfT21BdkJlZUg6MTpjaQ';
+  const { TWITTER_CLIENT_SECRET: clientSecret } = JSON.parse(secrets.SecretString);
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Basic ' + Buffer.from(`${clientId}:${clientSecret}`).toString('base64'),
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      code,
+      grant_type: 'authorization_code',
+      redirect_uri: redirectUrl,
+      code_verifier: verifier,
+    }),
+  });
 
-    const data = await response.text();
-    console.log('[data]', data);
-    resolve({
-      statusCode: 200,
-      body: data,
-    });
+  const json = await response.text();
+  console.log('[json]', json);
+  resolve({
+    statusCode: 200,
+    body: json,
   });
 };
